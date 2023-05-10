14555c175a3696a03acbed9dc2fa7ffdc1b86e98
tpettry <tpettry5449@gmail.com>
2023-05-09 22:01:18 -0700

    finished more functions

--- morefunctions.py
+++ morefunctions.py
@@ -3,3 +3,21 @@
 import functions
 
 functions.fib_2(10)
+
+## function can have multiple things that would turn into a tuple that you can unpack
+
+def sum_and_mult(a,b):
+    total = a + b
+    product = a * b
+    
+    return total, product
+
+print(sum_and_mult(1,4))
+
+
+# could have multiple values assign to many variables
+
+var_1, var_2 = sum_and_mult(6,7)
+
+print(var_1)
+print(var_2)
